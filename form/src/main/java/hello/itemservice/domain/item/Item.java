30445fb2d684ef2f3a805affea8cd0777cdcda1c
hongjunpyo11 <jpyo98@naver.com>
2022-12-13 14:42:46 +0900

    요구사항 추가

--- form/src/main/java/hello/itemservice/domain/item/Item.java
+++ form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,8 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 
@@ -9,6 +11,11 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    private Boolean open; // 판매 여부
+    private List<String> regions; // 등록지역
+    private ItemType itemType; // 상품 종류
+    private String deliveryCode; // 배송 방식
 
     public Item() {
     }
@@ -19,3 +26,7 @@ public class Item {
         this.quantity = quantity;
     }
 }
+
+/**
+ * ENUM , 클래스, String 같은 다양한 상황을 준비했다. 각각의 상황에 어떻게 폼의 데이터를 받을 수 있는지 하나씩 알아보자.
+ */
