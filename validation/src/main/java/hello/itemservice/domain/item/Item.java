b30941741fa72240d74f9bd1264eba03785f0537
hongjunpyo11 <jpyo98@naver.com>
2022-12-22 12:00:03 +0900

    Bean Validation - groups

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,18 +11,18 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
-    @NotNull // 수정 요구사항 추가
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank(message = "공백X") // 빈값 + 공백만 있는 경우를 허용하지 않는다.
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-//    @Max(9999) // 수정 요구사항 추가
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
@@ -41,4 +41,11 @@ public class Item {
  *
  * 결과적으로 item 은 등록과 수정에서 검증 조건의 충돌이 발생하고, 등록과 수정은 같은 BeanValidation 을 적용할 수 없다.
  * 이 문제를 어떻게 해결할 수 있을까?
+ *
+ * 방법 2가지
+ *   * BeanValidation의 groups 기능을 사용한다.
+ *     * 이런 문제를 해결하기 위해 Bean Validation은 groups라는 기능을 제공한다.
+ *       예를 들어서 등록시에 검증할 기능과 수정시에 검증할 기능을 각각 그룹으로 나누어 적용할 수 있다.
+ *
+ *   * Item을 직접 사용하지 않고, ItemSaveForm, ItemUpdateForm 같은 폼 전송을 위한 별도의 모델 객체를 만들어서 사용한다.
  */
