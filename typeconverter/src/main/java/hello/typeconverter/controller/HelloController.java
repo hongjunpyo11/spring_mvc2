19847b3d7d84d58f5c3b316d7029b510ff017d95
hongjunpyo11 <jpyo98@naver.com>
2022-12-28 10:08:05 +0900

    타입 컨버터 - Converter

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,30 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); // 문자 타입 조회
+        Integer intValue = Integer.valueOf(data); // 숫자 타입으로 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
+
+/**
+ * 문자를 숫자로 변환하거나, 반대로 숫자를 문자로 변환해야 하는 것 처럼
+ * 애플리케이션을 개발하다 보면 타입을 변환해야 하는 경우가 상당히 많다.
+ */
